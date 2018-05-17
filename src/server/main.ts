34062102a0b7bf066100f6c3092be145fa6d68b7
XeroSkill <mit_notelluc@hotmail.com>
2018-05-17 22:49:11 +1000

    Playing around with Electron and React with two tsconfigs and different module loaders

--- src/server/main.ts
+++ src/server/main.ts
@@ -11,7 +11,7 @@ function createWindow() {
     win = new electron.BrowserWindow({width: 800, height: 600});
 
     win.loadURL(url.format({
-        pathname: path.join(__dirname, 'index.html'),
+        pathname: path.join(__dirname, '../client/index.html'),
         protocol: 'file:',
         slashes: true
     }));
